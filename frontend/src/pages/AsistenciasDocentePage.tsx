942cb89865a5163ea64c476d42e4e3d984b2a848
Ricardo Cano <ricardocanoc06@gmail.com>
2026-06-30 17:24:07 -0300

    fix: convertir cursoId de string a number para CronogramaDocenteTab

--- frontend/src/pages/AsistenciasDocentePage.tsx
+++ frontend/src/pages/AsistenciasDocentePage.tsx
@@ -2677,7 +2677,7 @@ export function AsistenciasDocentePage({ onLogout, roles = [] }: Props) {
 
             {subView === 'cronograma' && cursoId ? (
               <div className="min-w-0">
-                <CronogramaDocenteTab cursoId={cursoId!} />
+                <CronogramaDocenteTab cursoId={Number(cursoId)} />
               </div>
             ) : null}
 
